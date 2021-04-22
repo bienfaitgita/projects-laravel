adb6e63d15eac58a6d8a631438ebbb4ff558761b
bgita <bgita.main@gmail.com>
2021-04-22 12:52:49 +0200

    Test

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
 
         <img src="{{asset('/images/drc_flag.png')}}" alt="drc_flag">
 
-       <h1 class="mt-5 text-5xl font-semibold text-indigo-600"> Hello from GOMA !</h1>
+       <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600"> Hello from GOMA !</h1>
 
        <p class="text-lg text-gray-600">It's currently {{ date('h:i A') }}</p>
 
